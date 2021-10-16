a0f4433fc8cf3c52c9675aafb621eecf2b1ea4ab
Nikita <moshkov.n.t@edu.mirea.ru>
2021-10-16 15:26:36 +0300

    last

---
+++ src/practika32/Movable.java
@@ -0,0 +1,8 @@
+package practika32;
+
+public interface Movable {
+    void moveUp();
+    void moveDown();
+    void moveRight();
+    void moveLeft();
+}
---
+++ src/practika62/Comparator.java
@@ -0,0 +1,3 @@
+package practika62;
+public interface Comparator { void sort(int marks[], int names[]); }
+
